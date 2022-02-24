ebc175fddcbcffdb4e5afb0f56742129a89f2461
zaynab-abdelnabi <zaynab.abdelnabi@gmail.com>
2022-02-24 12:27:26 +0200

    Step 4 : Strings Concatenation

---
+++ Step_4.php
@@ -0,0 +1,10 @@
+<?php 
+$name = "Zaynab";
+$position = "Web Developer";
+$github_url= "https://github.com/zaynab-abdelnabi";
+
+echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url.".\n";
+echo "Hello, I'm ",$name,", I'm a ",$position," please check my github link ",$github_url,".\n";
+echo "Hello, I'm ${name}, I'm a ${position} please check my github link ${github_url}.\n";
+
+?>
