9bb0ad195b23f01ee62e79368d490f885375d56d
zaynab-abdelnabi <zaynab.abdelnabi@gmail.com>
2022-02-24 12:03:41 +0200

    Step 3 : Variables lvl 1

---
+++ Step_3.php
@@ -0,0 +1,15 @@
+<?php
+$name = 'Zaynab Abd El Nabi';
+$email = 'zaynab.abdelnabi@gmail.com';
+
+echo "My name is ${name} and my email is ${email} .\n";
+
+$height = 15;
+$width = 20;
+
+$area = $height * $width;
+
+echo $area;
+echo "\n";
+
+?>
