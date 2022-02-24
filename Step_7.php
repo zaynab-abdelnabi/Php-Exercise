0be8d085d1e2ec935dea6695d0da1c3edfd826a6
zaynab-abdelnabi <zaynab.abdelnabi@gmail.com>
2022-02-24 14:44:33 +0200

    Step 7 :PHP Control Statements -  If / else

---
+++ Step_7.php
@@ -0,0 +1,11 @@
+<?php
+$birth_year = 2000;
+$current_year = 2022;
+
+if($current_year - $birth_year < 18){
+    echo "You still a kid, go and play GTA\n";
+}
+else{
+    echo "You can drive\n";
+}
+?>
