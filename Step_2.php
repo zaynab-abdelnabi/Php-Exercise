c1a2f9cc249a4ce88309264ed2b92d2b43d817fa
zaynab-abdelnabi <zaynab.abdelnabi@gmail.com>
2022-02-24 11:49:59 +0200

    Step 2 : Echo & Print

---
+++ Step_2.php
@@ -0,0 +1,8 @@
+Hello world, Help me I am using PHP
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+echo "I am getting errors but I can't see the errors\n";
+print "Search on how to enable PHP Error\n";
+?>
